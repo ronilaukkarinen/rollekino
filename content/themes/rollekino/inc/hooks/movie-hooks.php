51b6d3ce3984d5db5bf5c28eaf8062e288c4c8e3
Roni Laukkarinen <roni@dude.fi>
2021-08-24 23:31:52 +0300

    Fields etc.

--- content/themes/rollekino/inc/hooks/movie-hooks.php
+++ content/themes/rollekino/inc/hooks/movie-hooks.php
@@ -3,7 +3,7 @@
  * @Author: Roni Laukkarinen
  * @Date:   2021-02-04 18:15:59
  * @Last Modified by:   Roni Laukkarinen
- * @Last Modified time: 2021-02-04 18:19:44
+ * @Last Modified time: 2021-08-24 23:29:27
  *
  * @package rollekino
  */
@@ -13,17 +13,17 @@ namespace Air_Light;
 /**
  * Update movie title when saving article
  */
-add_filter( 'wp_insert_post_data' , __NAMESPACE__ . '\save_post_function', 10, 2);
+// add_filter( 'wp_insert_post_data', __NAMESPACE__ . '\save_post_function', 10, 2 );
 
-function save_post_function( $data, $id ) {
-  if ( 'post' != $data['post_type'] ) {
-    return;
-  }
+// function save_post_function( $data, $id ) {
+//   if ( 'post' === $data['post_type'] ) {
+//     // Save things in post meta
+//     update_post_meta( $id['ID'], '_rollekino_rating', '4' );
 
-  // Save things in post meta
-  update_post_meta( $id['ID'], '_rollekino_rating', '4' );
-
-  // Update the post's title.
-  $data['post_title'] = 'Test';
-  return $data;
-}
+//     // Update the post's title.
+//     $data['post_title'] = 'Test';
+//     return $data;
+//   } else {
+//     return $data;
+//   }
+// }
