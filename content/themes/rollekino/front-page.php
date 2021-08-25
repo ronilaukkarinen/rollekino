abe2acceb3a4959e6bc9b33583bbc5fb136c7ce1
Roni Laukkarinen <roni@dude.fi>
2021-08-25 20:22:51 +0300

    Start building movie metadata

--- content/themes/rollekino/front-page.php
+++ content/themes/rollekino/front-page.php
@@ -15,10 +15,38 @@
 
 namespace Air_Light;
 
-get_header(); ?>
+get_header();
+
+$movies = [];
+$post_type = 'movie';
+$args = [
+  'post_type' => $post_type,
+  'posts_per_page' => 10,
+];
+
+$query = new \WP_Query( $args );
+?>
 
 <main class="site-main">
-  <?php the_content(); ?>
+
+  <?php if ( ! empty( $query->posts ) ) : ?>
+    <section class="block block-movies">
+
+      <div class="container">
+        <?php while ( $query->have_posts() ) :
+          $query->the_post();
+
+          // Meta data
+          $rating = get_post_meta( get_the_ID(), 'rating', true );
+          $rating_imdb = get_post_meta( get_the_ID(), '_imdb_rating', true );
+          ?>
+
+          <h2><?php the_title(); ?></h2>
+        <?php endwhile; ?>
+      </div>
+
+    </section>
+  <?php endif; ?>
 </main>
 
 <?php get_footer();
--- content/themes/rollekino/inc/hooks/movie-hooks.php
+++ content/themes/rollekino/inc/hooks/movie-hooks.php
@@ -3,7 +3,7 @@
  * @Author: Roni Laukkarinen
  * @Date:   2021-02-04 18:15:59
  * @Last Modified by:   Roni Laukkarinen
- * @Last Modified time: 2021-08-24 23:29:27
+ * @Last Modified time: 2021-08-25 20:18:20
  *
  * @package rollekino
  */
@@ -13,17 +13,17 @@ namespace Air_Light;
 /**
  * Update movie title when saving article
  */
-// add_filter( 'wp_insert_post_data', __NAMESPACE__ . '\save_post_function', 10, 2 );
+ add_filter( 'wp_insert_post_data', __NAMESPACE__ . '\save_post_function', 10, 2 );
 
-// function save_post_function( $data, $id ) {
-//   if ( 'post' === $data['post_type'] ) {
-//     // Save things in post meta
-//     update_post_meta( $id['ID'], '_rollekino_rating', '4' );
+ function save_post_function( $data, $id ) {
+   if ( 'movie' === $data['post_type'] ) {
+     // Save things in post meta
+     update_post_meta( $id['ID'], '_imdb_rating', '7.5' );
 
-//     // Update the post's title.
-//     $data['post_title'] = 'Test';
-//     return $data;
-//   } else {
-//     return $data;
-//   }
-// }
+     // Update the post's title.
+     $data['post_title'] = 'Test';
+     return $data;
+   } else {
+     return $data;
+   }
+ }
