938fc50bb8745eadbc6cd9ae16619816a9e7fdbd
Roni Laukkarinen <roni@dude.fi>
2021-08-26 08:16:47 +0300

    Save more meta data

--- content/themes/rollekino/inc/hooks/movie-hooks.php
+++ content/themes/rollekino/inc/hooks/movie-hooks.php
@@ -3,7 +3,7 @@
  * @Author: Roni Laukkarinen
  * @Date:   2021-02-04 18:15:59
  * @Last Modified by:   Roni Laukkarinen
- * @Last Modified time: 2021-08-26 08:12:10
+ * @Last Modified time: 2021-08-26 08:16:13
  *
  * @package rollekino
  */
@@ -53,15 +53,33 @@ function save_post_function( $data, $id ) {
 
       $imdb_id = $ids[0][0];
       $movie = $omdb->getByImdbId( $imdb_id );
+
+      // https://github.com/rooxie/omdb-php
+      $imdb_title = $movie->getTitle();
+      $imdb_year = $movie->getYear();
       $imdb_rating = $movie->getImdbRating();
+      $imdb_release_date = $movie->getReleased();
+      $metascore_rating = $movie->getMetascore();
 
       // Save things in post meta
+      if ( ! metadata_exists( 'movie', $id['ID'], '_imdb_year' ) ) {
+        update_post_meta( $id['ID'], '_imdb_year', $imdb_year );
+      }
+
       if ( ! metadata_exists( 'movie', $id['ID'], '_imdb_rating' ) ) {
         update_post_meta( $id['ID'], '_imdb_rating', $imdb_rating );
       }
 
+      if ( ! metadata_exists( 'movie', $id['ID'], '_imdb_release_date' ) ) {
+        update_post_meta( $id['ID'], '_imdb_release_date', $imdb_release_date );
+      }
+
+      if ( ! metadata_exists( 'movie', $id['ID'], '_metascore_rating' ) ) {
+        update_post_meta( $id['ID'], '_metascore_rating', $metascore_rating );
+      }
+
       // Update the post's title.
-      $data['post_title'] = $movie->getTitle();
+      $data['post_title'] = $imdb_title;
 
       return $data;
     }
