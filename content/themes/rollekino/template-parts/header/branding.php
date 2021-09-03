544d4e4cca184a7bd391be8226d3fd8754176cf2
Roni Laukkarinen <roni@dude.fi>
2021-09-03 20:01:01 +0300

    Logo improvement

--- content/themes/rollekino/template-parts/header/branding.php
+++ content/themes/rollekino/template-parts/header/branding.php
@@ -15,10 +15,7 @@ $description = get_bloginfo( 'description', 'display' );
   <p class="site-title">
     <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
       <?php include get_theme_file_path( THEME_SETTINGS['logo'] ); ?>
-      <span>
-        <span class="bloginfo-name"><?php bloginfo( 'name' ); ?></span>
-        <span class="bloginfo-description"><?php bloginfo( 'description' ); ?></span>
-      </span>
+      <span class="screen-reader-text"><?php bloginfo( 'name' ); ?></span>
     </a>
   </p>
 
