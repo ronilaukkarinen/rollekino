a8b3c8f1be65dca9f09dcb03abf25c8584329ff0
Roni Laukkarinen <roni@dude.fi>
2021-09-05 20:32:42 +0300

    Small improvements

--- content/themes/rollekino/template-parts/loops-post.php
+++ content/themes/rollekino/template-parts/loops-post.php
@@ -2,8 +2,8 @@
 /**
  * @Author: Timi Wahalahti
  * @Date:   2021-06-11 14:43:07
- * @Last Modified by:   Timi Wahalahti
- * @Last Modified time: 2021-06-11 14:45:28
+ * @Last Modified by:   Roni Laukkarinen
+ * @Last Modified time: 2021-09-05 20:31:54
  *
  * @package rollekino
  */
@@ -15,10 +15,14 @@ if ( ! isset( $args['post_id'] ) ) {
 }
 ?>
 
-<h3>
-  <a href="<?php echo esc_url( get_the_permalink( $args['post_id'] ) ); ?>">
-    <?php echo esc_html( get_the_title( $args['post_id'] ) ) ?>
-  </a>
-</h3>
+<div class="result-post">
+  <h3>
+    <a href="<?php echo esc_url( get_the_permalink( $args['post_id'] ) ); ?>">
+      <?php echo esc_html( get_the_title( $args['post_id'] ) ) ?>
+    </a>
+  </h3>
 
-<?php echo wp_kses_post( get_the_excerpt( $args['post_id'] ) ); ?>
+  <p class="excerpt">
+    <?php echo wp_kses_post( get_the_excerpt( $args['post_id'] ) ); ?>
+  </p>
+</div>
