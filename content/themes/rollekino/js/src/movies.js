8fcdc4819eb5dbbc4d6ed9e252247db849521336
Roni Laukkarinen <roni@dude.fi>
2021-09-09 23:53:37 +0300

    Remove swup

--- content/themes/rollekino/js/src/movies.js
+++ content/themes/rollekino/js/src/movies.js
@@ -2,10 +2,6 @@
 /*
  * The movies archive
  */
-import Swup from 'swup';
-import SwupScriptsPlugin from '@swup/scripts-plugin';
-import SwupBodyClassPlugin from '@swup/body-class-plugin';
-
 import Vue from 'vue';
 
 // Import views
@@ -16,17 +12,6 @@ import Checkbox from './components/checkbox.vue';
 import FilterGroup from './components/filter-group.vue';
 import Sidebar from './components/sidebar.vue';
 import Pagination from './components/pagination.vue';
-
-// Initiate Swup transitions
-const swup = new Swup({
-  plugins: [
-    new SwupScriptsPlugin({
-      head: true,
-      body: true,
-    }),
-    new SwupBodyClassPlugin(),
-  ],
-});
 
 /**
  * Movies archive
@@ -50,30 +35,3 @@ const target = document.getElementById('movies-listing');
 if (target) {
   initMoviesArchive(target);
 }
-
-// Swup starts
-swup.on('contentReplaced', () => {
-  /**
- * Movies archive
- *
- * @param {Element} elem Target element
- */
-  const initMoviesArchive = (elem) => {
-    Vue.component('movies', Movies);
-    Vue.component('checkbox', Checkbox);
-    Vue.component('filter-group', FilterGroup);
-    Vue.component('sidebar', Sidebar);
-    Vue.component('pagination', Pagination);
-
-    return new Vue({
-      render: (h) => h(Movies),
-    }).$mount(elem);
-  };
-
-  const target = document.getElementById('movies-listing');
-
-  if (target) {
-    initMoviesArchive(target);
-  }
-});
-// Swup ends
