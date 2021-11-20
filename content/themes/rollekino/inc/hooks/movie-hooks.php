a941c45a832c43593e4acd2fe593b95273f81ec3
Roni Laukkarinen <roni@dude.fi>
2021-11-20 22:02:58 +0200

    Send notification of new movie review

--- content/themes/rollekino/inc/hooks/movie-hooks.php
+++ content/themes/rollekino/inc/hooks/movie-hooks.php
@@ -3,7 +3,7 @@
  * @Author: Roni Laukkarinen
  * @Date:   2021-02-04 18:15:59
  * @Last Modified by:   Roni Laukkarinen
- * @Last Modified time: 2021-10-30 17:30:03
+ * @Last Modified time: 2021-11-20 22:02:08
  *
  * @package rollekino
  */
@@ -104,6 +104,38 @@ function save_post_function_publish( $post_id ) {
       $wp_error = false,
       $fire_after_hooks = true
     );
+
+    // Settings
+    $telegram_bot_api_key = getenv( 'TELEGRAM_BOT_API_KEY' );
+    $method = 'sendMessage';
+    $chat_id = getenv( 'TELEGRAM_CHAT_ID' );
+$text = '🎥 Uusi leffa-arvio julkaistu: *' . get_the_title( $post_id ) . '*
+Linkki: ' . get_the_permalink( $post_id ) . '';
+
+    // API call
+    $url = 'https://api.telegram.org/bot' . $telegram_bot_api_key . '/' . $method . '?chat_id=' . $chat_id . '&text=' . $text . '&parse_mode=markdown';
+
+    // Get postdata
+    $postdata = get_post( $post_id );
+
+    // Get time difference
+    $time_differ = round( abs( strtotime( $postdata->post_modified ) - strtotime( $postdata->post_date ) ) / 60, 2 );
+
+    // Bail if other conditions not met
+    if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) return;
+    if ( wp_is_post_revision( $post_id ) ) return;
+    if ( wp_is_post_autosave( $post_id ) ) return;
+    if ( get_post_status( $post_id ) === 'draft' ) return;
+    if ( get_post_status( $post_id ) === 'auto-draft' ) return;
+    if ( get_post_status( $post_id ) === 'private' ) return;
+    if ( get_post_status( $post_id ) === 'future'  ) return;
+    if ( get_post_status( $post_id ) === 'pending' ) return;
+    if ( get_post_status( $post_id ) === 'trash' ) return;
+
+    // Send a message
+    if ( $time_differ < 0.10 ) {
+      wp_remote_get( $url );
+    }
 
     // Re-hook functions
     add_action( 'save_post', __NAMESPACE__ . '\save_post_function_publish' );
